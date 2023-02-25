65f4b0adda19a3405eec6b4ef4056c1c548ad38f
0xAbelheira <h.abelheira@gmail.com>
2023-02-25 15:47:42 +0000

    Almost done

---
+++ TPC2/tpc2.py
@@ -0,0 +1,33 @@
+linha = input("%")
+soma = 0
+
+aux = ""
+
+state = True
+aux_state = ""
+
+
+for c in linha:
+    c = c.upper()
+
+    if state == True:
+        if '0' <= c <= '9':
+            aux += c
+        else:
+            if len(aux) != 0:
+                soma += int(aux)
+                aux = ""
+
+    if c == "O":
+        aux_state = c
+    elif c == "N" and aux_state == "O":
+        state = True
+    elif c == "F" and aux_state == "OF":
+        state = False
+    elif c == "F" and aux_state == "O":
+        aux_state += c
+    elif c == "=":
+        print(soma)
+
+    
+
