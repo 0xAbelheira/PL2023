ab8a735c7485624f2a58942bcbb749c3e4693855
0xAbelheira <h.abelheira@gmail.com>
2023-03-05 17:19:33 +0000

    TPC3 complete

--- TPC3/tpc3.py
+++ TPC3/tpc3.py
@@ -1,6 +1,12 @@
 import re
 import json
 
+def print_dict(dict):
+    print(' ' * 50 + "| Frequência")
+    print('-' * 100)
+    for key in dict.keys():
+        print(key + ' ' * (50-len(key)) + "| " + str(dict[key]))
+
 def processes_per_year(lines):
     processes = dict()
 
@@ -43,71 +49,95 @@ def names_per_century(lines):
     return (first_names, last_names)
         
 
-def relationshiop_frequency(lines):
-    frequency = dict()
+def relationshiop_frequency(data):
+    relationship = dict()
 
-    for data in lines:
-        obs = data["obs"]
-        if match := re.search(r"?i:irmao|irma|pai|mae|tio|tia|avo|primo|prima|sobrinho|sobrinha", obs):
-            relation = match.group().lower()
+    for process in data:
+        obs = process["obs"]
+        if match := re.search(r"(?i)[a-zA-Z ],([A-Z][a-zA-Z ]*)\.", obs):
+            relation = match.group(1)
             
-            if relation not in frequency:
-                frequency[relation] = 0
+            if relation not in relationship:
+                relationship[relation] = 0
+            relationship[relation] += 1
+    
+    return relationship
 
-            frequency[relation] += 1
+def most_used_names_per_sec(first_names, last_names):
+    first_name_keys_sorted = list(first_names.keys())
+    last_name_keys_sorted = list(last_names.keys())
 
-    return frequency
+    first_name_keys_sorted.sort()
+    last_name_keys_sorted.sort()
+    
+    print("================================================================")
+    print("                         First names")
+    print("================================================================")
+    for sec in first_name_keys_sorted:
+        print('-' * 20)
+        print("     Século " + str(sec))
+
+        items = list(first_names[sec].items())
+        items.sort(key=lambda x : x[1], reverse=True)
+
+        for i in range(0, 5):
+            print(items[i])
 
 
-def processes_to_json(lines):
-    first_twenty = lines[:20]
+    print("================================================================")
+    print("                         Last names")
+    print("================================================================")
+    for sec in last_name_keys_sorted:
+        print('-' * 20)
+        print("     Século " + str(sec))
 
-    file = open("registos.json", "w")
-    json.dump(first_twenty, file)
+        items = list(last_names[sec].items())
+        items.sort(key=lambda x : x[1], reverse=True)
+
+        for i in range(0, 5):
+            print(items[i])
+
+def processes_to_json(data, path):
+    file = open(path + ".json", "w")
+    json.dump(data[:20], file)
     file.close()
 
 def main():
     file = open("processos.txt")
+    regex = re.compile(r"(?P<folder>\d+)::(?P<year>\d{4})\-(?P<month>\d{2})-(?P<day>\d{2})::(?P<name>[A-Za-z ]+)::(?P<f_name>[A-Za-z ]+)::(?P<m_name>[A-Za-z ]+)::(?P<obs>.*)::")
+
     valid_lines = list()
-
-    regex = re.compile(r"(?P<folder>\d+)::(?P<year>\d{4})\-(?P<month>\d{2})-(?P<day>\d{2})::(?P<name>[A-Za-z ]+)::(?P<f_name>[A-Za-z ]+)::(?P<m_name>[A-Za-z ]+)::(?P<obs>[^:]*)::")
-
     matches = regex.finditer(file.read())
-
     for match in matches:
         valid_lines.append(match.groupdict())
 
-    processes_to_json(valid_lines)
-
     option = 0
     while option != 5:
-        print('\n')
-        print('Escolha uma opção')
-        print('-----------------------------------------')
-        print('1 - Frequência de processos por ano')
-        print('2 - Frequência de nomes próprios e apelidos')
-        print('3 - Frequência dos vários tipos de relação')
-        print('4 - Converter os 20 primeiros registos para formato Json')
-        print('5 - Sair')
-        print('-----------------------------------------')
+        print("------------------------------------------")
+        print("1 - Frequência de processos por ano")
+        print("2 - Frequência de nomes por século")
+        print("3 - Frequência de tipos de relação")
+        print("4 - 20 primeiros registos para json")
+        print("5 - Sair")
+        print("------------------------------------------")
+
         option = int(input())
 
         match option:
             case 1:
-                print(processes_per_year(valid_lines))
-                
+                print_dict(processes_per_year(valid_lines))
+
             case 2:
-                print(names_per_century(valid_lines))
-
+                first_names, last_names = names_per_century(valid_lines)
+                most_used_names_per_sec(first_names, last_names)
             case 3:
-                print(relationshiop_frequency(valid_lines))
-
+                print_dict(relationshiop_frequency(valid_lines))
             case 4:
-                open("registos.json")
-
-            case 5: 
+                print("Nome do ficheiro:")
+                path = input()
+                processes_to_json(valid_lines, path)
+            case 5:
                 print("A sair...")
-
             case _:
                 print("Opção inválida!")
     
